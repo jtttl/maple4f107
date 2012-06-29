1110f5420c958509c9297087fcd34220ed28cd4e
wangchen2011@gmail.com <wangchen2011@gmail.com@22657575-f6bc-b298-15ed-954e9b1293d1>
2012-06-29 15:49:49 +0000

    移植start01成功

--- app/main.c
+++ app/main.c
@@ -1,129 +1,64 @@
-#include <stdio.h>
-#include "stm32f10x_conf.h"
+//#include "main.h"	 //程序资源
+#include "includes.h"
+#include "startup.h"
+#include "serial.h"
+#include "timer.h"
 
-void delay(int n)
-{
-	while(n-- > 0)
-	{
-		asm volatile ("nop");
+
+//======================================【任务定义】==================================
+#define  StartJob_STK_SIZE		128      	//启动任务堆栈大小
+#define  SYSCLK_FREQ_72MHz  72000000
+static   BOOL SysClockCheck(void);
+
+
+
+//任务函数
+void StartJob			(void);			//启动任务
+			  
+//======================================【引入变量】==================================
+extern u8 HardFault_Handler_T;	//找错
+
+//======================================【系统参数】==================================
+//初始化硬件，初始化系统，建立启动任务				
+int main(void)
+{	
+
+	BSP_Init();
+	InitSystem();
+	if(!SysClockCheck()){
+		DebugPf("System Check Failed\r\n");
+		while(1){}
 	}
-}
-void flash_test()
-{
-	static int r = 0;
-	if(r){
-		GPIO_SetBits(GPIOE,GPIO_Pin_2);
-	}else{
-		GPIO_ResetBits(GPIOE,GPIO_Pin_2);
-	}
-	r = !r;
-}
-void USART1_IRQHandler(void)
-{
-	if(USART_GetITStatus(USART1,USART_IT_RXNE) == SET)
-	{
-		USART_ClearITPendingBit(USART1,USART_IT_RXNE);
-		char c = USART_ReceiveData(USART1);
-		USART_SendData(USART1,c);
-		while(USART_GetFlagStatus(USART1,USART_FLAG_TXE) == RESET);
-	}
-}
-void usart1_nvic_config()
-{
-	NVIC_InitTypeDef nvic_cfg;
-
-	NVIC_SetVectorTable(NVIC_VectTab_FLASH,0x0);
-
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-
-	nvic_cfg.NVIC_IRQChannel = USART1_IRQn;
-	nvic_cfg.NVIC_IRQChannelPreemptionPriority = 0;
-	nvic_cfg.NVIC_IRQChannelSubPriority = 1;
-	nvic_cfg.NVIC_IRQChannelCmd = ENABLE;
-
-	NVIC_Init(&nvic_cfg);
-}
-void usart1_init()
-{
-	GPIO_InitTypeDef GPIO_InitStructure;		//串口引脚结构
-	USART_InitTypeDef uart_cfg;
-
-	usart1_nvic_config();
-
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_AFIO, ENABLE);
-
-	//init usart1's gpio
-	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_9; //tx
-	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_10; //rx
-	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IN_FLOATING;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-
-	//init usart1 module
-	uart_cfg.USART_BaudRate = 115200;
-	uart_cfg.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	uart_cfg.USART_Parity = USART_Parity_No;
-	uart_cfg.USART_StopBits = USART_StopBits_1;
-	uart_cfg.USART_WordLength = USART_WordLength_8b;
-	uart_cfg.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-	USART_Init(USART1,&uart_cfg);
-
-	USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
-	USART_Cmd(USART1,ENABLE);
-
-
-}
-void usart1_putc(char ch)
-{
-	USART_SendData(USART1,ch);
-	while(USART_GetFlagStatus(USART1,USART_FLAG_TXE) == RESET);
-}
-int usart1_send(const char* text)
-{
-	while(*text != 0){
-		usart1_putc(*text);
-		text++;
-	}
+	StartJob();
 	return 0;
 }
-void led_init()
+static BOOL SysClockCheck(void)
 {
-	GPIO_InitTypeDef GPIO_InitStructure;		//串口引脚结构
-
-	//SystemInit();
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
-
-
 	
-	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_2|GPIO_Pin_3;	
-	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;	
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	
-	
-	GPIO_Init(GPIOE, &GPIO_InitStructure);
+	RCC_ClocksTypeDef RCC_ClockFreq;
+
+	RCC_GetClocksFreq(&RCC_ClockFreq);	//为时钟变量赋予片上各时钟
+
+	if(SYSCLK_FREQ_72MHz != RCC_ClockFreq.SYSCLK_Frequency)	//SYSTEM频率
+	{
+		return false;
+	}	
+	return true;
 }
-void bsp_init()
+
+//extern void anybus_service(void);
+//启动任务============================================================================
+void StartJob(void ) 
 {
-	led_init();
-	usart1_init();
-}
-int a,b;
-int main()
-{
-	bsp_init();
+	start_sys_timer();
 	while(1)
 	{
-		flash_test();
-		delay(0x500000);
-		printf("hello world\r\n");
+		timer_srv();
+		#if (DEVICE_NETWORK_EN > 0)
+			//network_srv();
+		#endif
+		//anybus_service();
 	}
-	return 0;
+
 }
+
